02d0610521431212f3963d81b950d1f8d5169f2d
ash-pote <poteashleigh@gmail.com>
2025-10-07 15:01:58 +0100

    Removed facemesh

--- sketch.js
+++ sketch.js
@@ -14,25 +14,14 @@ let hands = [];
 let previousX = null;
 let previousY = null;
 
-let faceMesh;
-let options = { maxFaces: 1, refineLandmarks: false, flipped: false };
-let faces = [];
-
 function preload() {
   handPose = ml5.handPose();
-  faceMesh = ml5.faceMesh(options);
 }
 
 // Callback function for when handPose outputs data
 function gotHands(results) {
   // Save the output to the hands variable
   hands = results;
-}
-
-// Callback function for when faceMesh outputs data
-function gotFaces(results) {
-  // Save the output to the faces variable
-  faces = results;
 }
 
 function setup() {
@@ -46,22 +35,11 @@ function setup() {
 
   // Start detecting hands from the webcam video
   handPose.detectStart(video, gotHands);
-  faceMesh.detectStart(video, gotFaces);
 }
 
 function draw() {
   image(video, 0, 0, width, height);
-
-  // Draw all the tracked face points
-  for (let i = 0; i < faces.length; i++) {
-    let face = faces[i];
-    for (let j = 0; j < face.keypoints.length; j++) {
-      let keypoint = face.keypoints[j];
-      fill(0, 255, 0);
-      noStroke();
-      circle(keypoint.x, keypoint.y, 5);
-    }
-  }
+  filter(INVERT);
 
   // Draw all the tracked hand points
   for (let i = 0; i < hands.length; i++) {
